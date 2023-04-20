fc8d760be1eec8cf3f5c1e267bc6d14a537e909a
Olga <komiagina.olya@yandex.ru>
2023-04-20 20:17:43 +0300

    Решила 1 задачу 3 семинара

--- Seminar 1.py/Seminar 2.py/Task 5.py
+++ Seminar 1.py/Seminar 2.py/Task 5.py
@@ -1,14 +1,14 @@
-n = int(input("Введите количество монет: "))
-orel = reshka = 0
-for i in range(n):
-    x = int(input("Орел(1) или Решка(0)? "))
-    if x == 1:
-        orel += 1
-    else:
-        reshka += 1
-if orel < reshka:
-    print(f"Переверните {orel} монет с орла на решку, их меньше всего")
-elif orel == reshka:
-    print(f"Количество орлов и решек одинаково, по {orel} штук")
-else:
-    print(f"Переверните {reshka} монет с решки на орла, их меньше всего")
+#n = int(input("Введите количество монет: "))
+#orel = reshka = 0
+#for i in range(n):
+#    x = int(input("Орел(1) или Решка(0)? "))
+#   if x == 1:
+#       orel += 1
+#   else:
+#       reshka += 1
+#if orel < reshka:
+#   print(f"Переверните {orel} монет с орла на решку, их меньше всего")
+#elif orel == reshka:
+#    print(f"Количество орлов и решек одинаково, по {orel} штук")
+#else:
+#    print(f"Переверните {reshka} монет с решки на орла, их меньше всего")
