6e13b531191fc54993b31f475273d408c9572814
Olga <komiagina.olya@yandex.ru>
2023-04-27 20:56:50 +0300

    Решила 1 задачу 5 семинара

---
+++ Seminar 5.py/Task 13.py
@@ -0,0 +1,18 @@
+#Задача 26:  Посчитать факториал (произведение 1 до N) и треугольное число (сумма чисел от 1 до N) для числа N ЧЕРЕЗ РЕКУРСИЮ и без циклов
+
+#def factorial(n):
+#    if (n <= 1):
+#        return 1
+#    else:
+#        return (n * factorial(n - 1))
+    
+#def triangularNumber(n):
+#    if (n == 1):
+#        return 1
+#    else:
+#        return triangularNumber(n - 1) + n
+#n = int(input("Введите число: "))
+#print("Факториал числа равен: ")
+#print(factorial(n))
+#print("Треугольное число равно: ")
+#print(triangularNumber(n))
